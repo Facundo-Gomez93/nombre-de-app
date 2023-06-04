3e66f45f6e67748fd9a7a01a62b83682fde7e949
Facundo-Gomez93 <facugomez61193@gmail.com>
2023-06-03 22:13:37 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from "./componentes/navBar/navBar/navBar";
+import itemListContainer from "./componentes/itemListContainer/itemListContainer";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div>
+          <itemListContainer greeting="Bienvenidos al carrito de compras" />
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
